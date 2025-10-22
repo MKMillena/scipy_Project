6e222b88273dd87e22745b40e0940f393fefe970
MKMillena <milena.p.r.17@gmail.com>
2025-10-22 13:00:09 -0300

    Add hello.py to display library versions

---
+++ hello.py
@@ -0,0 +1,8 @@
+import numpy
+import scipy
+import sympy
+
+print("Script executado com sucesso!")
+print(f"Versão do NumPy: {numpy.__version__}")
+print(f"Versão do SciPy: {scipy.__version__}")
+print(f"Versão do SymPy: {sympy.__version__}")
